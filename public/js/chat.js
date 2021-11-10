9c9392b0e87be9162cbb7e92cb0f07017512c454
Hassan-AlAmer <hassan.m.809@gmail.com>
2021-11-10 23:35:17 +0300

    Finish design chat by using Bootstrap

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,16 @@
+// Make connection
 const socket = io();
+ 
+var NickNameModal = new bootstrap.Modal(document.getElementById('NickNameModal'));
+NickNameModal.show();
+
+var NickName=document.getElementById("NickName");
+var NickNameButton=document.getElementById("NickNameButton");
+
+NickNameButton.addEventListener('click',()=>{
+    if(NickName.value.length == 0){
+        document.getElementById("NickName-error").innerText="Please Enter Your NickName";
+    }else{
+        NickNameModal.hide();
+    }
+})
